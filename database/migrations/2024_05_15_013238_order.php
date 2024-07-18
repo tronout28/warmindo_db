4085175d6bac883eba810a72b66859e8e9550fa5
tronout28 <fatihabdurahman28@gmail.com>
2024-07-18 07:40:37 +0700

    fix:[FA] fix order migration

--- database/migrations/2024_05_15_013238_order.php
+++ database/migrations/2024_05_15_013238_order.php
@@ -14,18 +14,16 @@ return new class extends Migration
     public function up()
     {
         Schema::create('orders', function (Blueprint $table) {
-            $table->id('order_id');
+            $table->id(); // Menggunakan kolom 'id' sebagai primary key
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->unsignedBigInteger('menuID');
             $table->foreign('menuID')->references('menuID')->on('menus')->onDelete('cascade');
-            $table->decimal('price_order');
+            $table->decimal('price_order', 8, 2); // Tentukan panjang dan presisi decimal
             $table->timestamp('order_date')->useCurrent();
-            $table->string('status');
-            $table->decimal('payment');
+            $table->enum('status', ['selesai', 'sedang diproses', 'batal', 'pesanan siap', 'menunggu batal']);
+            $table->decimal('payment', 8, 2); // Tentukan panjang dan presisi decimal
             $table->boolean('refund')->default(false);
             $table->text('note')->nullable();
-            $table->enum('status',
-                ['selesai', 'sedang diproses', 'batal', 'pesanan siap', 'menunggu batal'])->change();
             $table->timestamps();
         });
     }
--- database/migrations/2024_07_18_002550_create_order_details_table.php
+++ database/migrations/2024_07_18_002550_create_order_details_table.php
@@ -4,22 +4,34 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOrderDetailsTable extends Migration
+return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
     public function up()
     {
         Schema::create('order_details', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('order_id')->constrained('orders')->onDelete('cascade');
-            $table->foreignId('menuID')->constrained('menus');
+            $table->unsignedBigInteger('order_id');
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->unsignedBigInteger('menuID');
+            $table->foreign('menuID')->references('menuID')->on('menus');
             $table->integer('quantity');
             $table->decimal('price', 8, 2);
             $table->timestamps();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
     public function down()
     {
         Schema::dropIfExists('order_details');
     }
-}
+};
