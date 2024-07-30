8a8ac01450ba27c79d06653cb6d810f066e656e9
tronout28 <fatihabdurahman28@gmail.com>
2024-07-30 09:33:15 +0700

    feat: [FA] admin update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -36,28 +36,96 @@ class AdminController extends Controller
     public function register(Request $request)
     {
         $request->validate([
+            'name' => 'required|string|max:255',
             'username' => 'required|string|max:255',
-            'name' => 'required|string|max:255', 
+            'profile_picture' => 'nullable|string|max:255', 
             'email' => 'required|email',
-            'phone' => 'required|string',
+            'phone_number' => 'required|string',
             'password' => 'required|string|min:8',
         ]);
-        $user = Admin::where('email', $request->email)->first();
-        if ($user != null) {
+        $admin = Admin::where('email', $request->email)->first();
+        if ($admin != null) {
             return response([
                 'message' => 'Email already exists',
             ], 409);
         }
-        $userdata = [
+        $admindata = [
+            'name' => $request->username,
             'username' => $request->username,
-            'name' => $request->name, 
+            'profile_picture' => $request->profile_picture, 
             'email' => $request->email,
             'phone_number' => $request->phone,
             'password' => Hash::make($request->password),
             'role' => 'admin',
         ];
-        $user = Admin::create($userdata);
+        $admin = Admin::create($admindata);
+        $token = $admin->createToken('warmindo')->plainTextToken;
+
+        return response(['admin' => $admin,
+            'token' => $token,
+        ], 201);
+    }
+
+    public function update (Request $request)
+    {
+        $request->validate([
+            'name' => 'nullable|string|max:255',
+            'username' => 'nullable|string|max:255',
+            'email' => 'nullable|email',
+            'profile_picture' => 'nullable|string|max:255',
+            'phone' => 'nullable|string',
+            'password' => 'nullable|string|min:8',
+            'current_password' => 'sometimes|nullable|string|min:8', 
+        ]);
+        
+        $user = Admin::where('email', $request->email)->first();
+        if ($user == null) {
+            return response([
+                'message' => 'Email not found',
+            ], 404);
+        }
+        $userdata = [
+            'username' => $request->username,
+            'name' => $request->name,
+            'profile_picture' => $request->profile_picture,
+            'email' => $request->email,
+            'phone_number' => $request->phone,
+            'password' => Hash::make($request->password),
+            'role' => 'admin',
+        ];
+        $user->update($userdata);
         $token = $user->createToken('warmindo')->plainTextToken;
+
+        if ($request->hasFile('profile_picture')) {
+            // Delete old picture if exists
+            if ($user->profile_picture) {
+                $oldImagePath = public_path('images') . '/' . $user->profile_picture;
+                if (file_exists($oldImagePath)) {
+                    unlink($oldImagePath);
+                }
+            }
+            $imageName = env('APP_URL') . time().'.'.$request->profile_picture->extension();
+            Admin::where('Uploading picture profile: '.$imageName);
+            $request->profile_picture->move(public_path('images'), $imageName);
+            $user->profile_picture = $imageName;
+        }
+
+        // Validasi current_password dan perbarui password jika valid
+        if ($request->filled('current_password') || $request->filled('password')) {
+            $request->validate([
+                'current_password' => 'required|string|min:8',
+                'password' => 'required|string|min:8|confirmed',
+            ]);
+
+            if (!Hash::check($request->current_password, $user->password)) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Current password is incorrect',
+                ], 400);
+            }
+
+            $user->password = Hash::make($request->password);
+        }
 
         return response(['admin' => $user,
             'token' => $token,
@@ -65,44 +133,44 @@ class AdminController extends Controller
     }
 
     public function verifyUser($id)
-{
-    $user = User::find($id);
+    {
+        $user = User::find($id);
 
-    if (!$user) {
+        if (!$user) {
+            return response()->json([
+                'message' => 'User not found',
+            ], 404);
+        }
+
+        // Example: check a condition or separate method for verification logic
+        $user->user_verified = true;
+        $user->save();
+
         return response()->json([
-            'message' => 'User not found',
-        ], 404);
+            'message' => 'User verified successfully',
+            'user' => $user,
+        ], 200);
     }
 
-    // Example: check a condition or separate method for verification logic
-    $user->user_verified = true;
-    $user->save();
+    public function unverifyUser($id)
+    {
+        $user = User::find($id);
 
-    return response()->json([
-        'message' => 'User verified successfully',
-        'user' => $user,
-    ], 200);
-}
+        if (!$user) {
+            return response()->json([
+                'message' => 'User not found',
+            ], 404);
+        }
 
-public function unverifyUser($id)
-{
-    $user = User::find($id);
+        // Unverify the user
+        $user->user_verified = false;
+        $user->save();
 
-    if (!$user) {
         return response()->json([
-            'message' => 'User not found',
-        ], 404);
+            'message' => 'Now user is not verified',
+            'user' => $user,
+        ], 400);
     }
-
-    // Unverify the user
-    $user->user_verified = false;
-    $user->save();
-
-    return response()->json([
-        'message' => 'Now user is not verified',
-        'user' => $user,
-    ], 400);
-}
 
     
     public function logout()
